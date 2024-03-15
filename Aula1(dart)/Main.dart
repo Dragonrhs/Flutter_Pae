6f55e3350a1fa0c3f0c6b45c5232d5f7de8a5353
Ryuske <Ryuske2009@hotmail.com>
2024-03-15 13:57:24 -0300

    <feat>[]:operaçoes

--- Aula1(dart)/Main.dart
+++ Aula1(dart)/Main.dart
@@ -8,8 +8,10 @@ main(){
 
   // print(nome);
 
-  // nome = 123; //nao da erro, pois a variavel nao esta tipada ele tipa na memoria 
+  // nome = 123; //nao da erro, pois a variavel nao esta tipada ele tipa na memoria
+
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
   const int idade = 20; //constante
   String nome = "Ryuske";
   //int idade = 20;
@@ -18,6 +20,16 @@ main(){
 
   print(nome + " tem " + idade.toString() + " anos e " + altura.toString() + " de altura." + " Estudante: " + estudante.toString());
 
-  
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+  //operaçoes
+  int a = 10; 
+  int b = 2;
+  var soma = a + b; 
+  var sub = a - b;
+  var mult = a * b;
+  var div = a / b;
+
+  print("resultado da soma: " + soma.toString() + " resultado da subtração: " + sub.toString() + " resultado da multiplicação: " + mult.toString() + " resultado da divisão: " + div.toString();
 
 }
