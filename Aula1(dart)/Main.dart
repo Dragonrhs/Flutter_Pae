c4e28ecfbd9b155d5b5ed9d6642aeaf7b09dc695
Ryuske <Ryuske2009@hotmail.com>
2024-03-15 13:47:37 -0300

    Variaveis primitivas

---
+++ Aula1(dart)/Main.dart
@@ -0,0 +1,21 @@
+
+main(){
+    //Variaveis primitivas
+  // var nome = "Ryuske";
+  // var idade = 20;  //nao esta tipando 
+  // var altura = 1.71;
+  // var estudante = true;
+
+  // print(nome);
+
+  // nome = 123; //nao da erro, pois a variavel nao esta tipada ele tipa na memoria 
+
+  String nome = "Ryuske";
+  int idade = 20;
+  double altura = 1.71;
+  bool estudante = true;
+
+  print(nome + " tem " + idade.toString() + " anos e " + altura.toString() + " de altura.");
+
+  
+}
