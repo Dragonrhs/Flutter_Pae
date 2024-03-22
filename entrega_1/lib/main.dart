998deb99468d3348fd714ef6147881109342607f
Ryuske <Ryuske2009@hotmail.com>
2024-03-22 14:39:57 -0300

    <feat>[]: criaçao do repo e da home page

---
+++ entrega_1/lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(Homepage());
+
+}
+
+
+class Homepage extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Home page"),
+        backgroundColor: const Color.fromRGBO(125, 96, 161, 1),
+      
+      ),
+      body: 
+
+    )
+}
+}
