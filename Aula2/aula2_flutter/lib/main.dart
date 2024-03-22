9d8a132b988ea54cd868bd412a697d10f5ad1616
Ryuske <Ryuske2009@hotmail.com>
2024-03-22 14:06:24 -0300

    <feat>[]: Criaçao do app bar e do body com widgets

--- Aula2/aula2_flutter/lib/main.dart
+++ Aula2/aula2_flutter/lib/main.dart
@@ -9,26 +9,45 @@ main(){
 //criaçao de widgets
 class PrimeiroApp extends StatelessWidget {
 
+  // const PrimeiroApp({Key? key})
+
   @override  //builda a tela e mostra para o usuario
   Widget build(BuildContext context) {
     //primeiro widget q tem q colocar na hierarquia de widgets
-    return MaterialApp(
+    return const MaterialApp(
 
       title: "Meu Primeiro App",
       home: Homepage(),
+
     );
     
 }
 }
 
 class Homepage extends StatelessWidget{
+ 
+
+  const Homepage({super.key});
 
   @override
   Widget build(BuildContext context) {
     
-    return Container(
-      color: Colors.blue
-    );
+    // return Container(
+    //   color: Colors.blue
+    // );
+  return Scaffold(
+    appBar: AppBar(
+      title: const Text("Home page"),
+      backgroundColor: Colors.black,
+      leading: const Icon(Icons.person),
+    ),
 
+    body: const Center(
+      child: Text("Olá, mundo!"),
+    ),
+  
+  );
+    
+  
 }
 }
