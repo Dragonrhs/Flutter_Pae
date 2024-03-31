e35a867ce568c0165b696867f6f0cc8a28628322
Ryuske Hideaki Sato <ryuske2009@hotmail.com>
2024-03-30 22:56:08 -0300

    <feat>[]: template basico sem botao lateral

--- entrega_1/lib/main.dart
+++ entrega_1/lib/main.dart
@@ -17,11 +17,54 @@ class Homepage extends StatelessWidget{
           backgroundColor: const Color.fromRGBO(125, 96, 161, 1),
         
         ),
-        body: const Text("teste")
-
-      )
+        body: Container(
+          color: Colors.white,
+          padding: EdgeInsets.all(16),
+          child: Column(
+            children: [
+              Container(
+                padding: EdgeInsets.all(16),
+                decoration: BoxDecoration(
+                  color: const Color.fromARGB(255, 255, 255, 255),
+                  borderRadius: BorderRadius.circular(8),
+                ),
+                child: Text(
+                  'Every Purchase Will Be Made With Pleasure',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              SizedBox(height: 16),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  ElevatedButton(
+                    onPressed: () {},
+                    child: Text('Buy and Enjoy'),
+                  ),
+                  ElevatedButton(
+                    onPressed: () {},
+                    child: Text('Start Shopping'),
+                  ),
+                  ElevatedButton(
+                    onPressed: () {},
+                    child: Text('Learn More'),
+                  ),
+                  ElevatedButton(
+                    onPressed: () {},
+                    child: Text('Login'),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
     );
-}
+  }
 }
 
 
