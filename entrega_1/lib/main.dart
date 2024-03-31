e649861b4f33a808e53d36553b1ecd9d2c782ee9
Ryuske Hideaki Sato <ryuske2009@hotmail.com>
2024-03-31 14:40:16 -0300

    <feat>[]: adiçao de estilos e childs

--- entrega_1/lib/main.dart
+++ entrega_1/lib/main.dart
@@ -17,52 +17,99 @@ class Homepage extends StatelessWidget{
           backgroundColor: const Color.fromRGBO(125, 96, 161, 1),
         
         ),
-        body: Container(
-          color: Colors.white,
-          padding: EdgeInsets.all(16),
-          child: Column(
-            children: [
-              Container(
-                padding: EdgeInsets.all(16),
-                decoration: BoxDecoration(
-                  color: const Color.fromARGB(255, 255, 255, 255),
-                  borderRadius: BorderRadius.circular(8),
-                ),
-                child: const Text(
-                  'Every Purchase Will Be Made With Pleasure',
-                   style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 24,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
+        body:Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text("Every Purchase\nWill be Made\nWith Pleasure",
+              style:TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 50,
+              fontStyle:FontStyle.normal,
               ),
-              const SizedBox(height: 16),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  ElevatedButton(
-                    onPressed: () {},
-                    child: Text('Buy and Enjoy'),
-                  ),
-                  ElevatedButton(
-                    onPressed: () {},
-                    child: Text('Start Shopping'),
-                  ),
-                  ElevatedButton(
-                    onPressed: () {},
-                    child: Text('Learn More'),
-                  ),
-                  ElevatedButton(
-                    onPressed: () {},
-                    child: Text('Login'),
-                  ),
-                ],
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(height: 5),
+            const Text("Buy and Enjoy",
+              textAlign: TextAlign.center,
+              style:TextStyle(
+              fontWeight: FontWeight.normal,
+              fontSize: 16,
+              fontStyle:FontStyle.normal,
               ),
-            ],
-          ),
+            ),
+            const SizedBox(height: 15),
+            ElevatedButton(
+              onPressed: () {
+              },
+              style:const ButtonStyle(
+                backgroundColor: MaterialStatePropertyAll( Color.fromRGBO(125, 96, 161, 1)),
+              ),
+              child: const Text("Start Shopping",
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 16,
+                      fontStyle:FontStyle.normal,
+                      fontWeight: FontWeight.bold)),
+            ),
+            const SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                ElevatedButton(
+                  onPressed: () {
+                  },
+                  style:ButtonStyle(
+                    shape: MaterialStateProperty.all(RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(18.0),
+                      side: const BorderSide(width: 3,color:  Color.fromRGBO(125, 96, 161, 1))
+                    ))
+                  ),
+                  child: const Text("Learn More",
+                      style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 16,
+                          fontStyle:FontStyle.normal,
+                          fontWeight: FontWeight.bold)),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                  },
+                  style:ButtonStyle(
+                    shape: MaterialStateProperty.all(RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(18.0),
+                      side: const BorderSide(width: 3,color: Color.fromRGBO(125, 96, 161, 1))
+                    ))
+                  ),
+                  child: const Text("  Login  ",
+                      style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 16,
+                          fontStyle:FontStyle.normal,
+                          fontWeight: FontWeight.bold)),
+                ),
+              ],
+            ),
+          ],
         ),
       ),
+      bottomNavigationBar: BottomAppBar(
+        color: Colors.white,
+        child:
+          Align(
+            alignment: Alignment.bottomRight,
+            child: Container(
+              height: 46,
+              width: 46,
+              decoration: const BoxDecoration(
+                shape: BoxShape.circle,
+                color: Color.fromRGBO(125, 96, 161, 1),
+              ),
+              child: const Icon(Icons.home,color: Colors.white,),
+              ),
+          ),
+      ),
+    )
     );
   }
 }
