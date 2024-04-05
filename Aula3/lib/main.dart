5b8e0c52e6809456c559d3b000d87b0443e2fb64
Ryuske <Ryuske2009@hotmail.com>
2024-04-05 14:22:23 -0300

    <feat>[]: criaçao do body

--- Aula3/lib/main.dart
+++ Aula3/lib/main.dart
@@ -37,14 +37,62 @@ class Homepage extends StatelessWidget {
           SizedBox(width: 16,),
         ],
       ),
-      body: const Column(
-        children: [
-          Text("Aprenda flutter no seu tempo"),
-          Text("Bora aprender Flutter! Compre meu curso por R\$22,90.Qualidade garantida"),
-        ],
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){},
+        backgroundColor: Colors.black,
+        child: const Icon(Icons.home,size: 48,),
+      ),
+      body:const Center(
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 12),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text("Aprenda flutter no seu tempo", 
+              style: TextStyle(
+                fontSize: 48,
+                fontWeight: FontWeight.bold,
+                color: Colors.black,
+              ),
+              ),
+              SizedBox(height: 8,),
+              Text("Bora aprender Flutter! Compre meu curso por R\$22,90.Qualidade garantida",
+              style: TextStyle(
+                fontSize: 20,
+                color: Colors.black,
+              ),
+              ),
+              SizedBox(height: 16,),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children:  [
+                  Starwidget(),
+                  Starwidget(),
+                  Starwidget(),
+                ],
+              ),
+            ],
+            ),
         ),
+      ),
 
     );  
     
+  }
+}
+
+
+class Starwidget extends StatelessWidget {
+  const Starwidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return  const Column(
+        children: [
+          Icon(Icons.star, color: Colors.yellow, size: 48,),
+          Text("+45.00 alunos", style: TextStyle(fontSize: 20),),
+          Text("Didatica garantida", style: TextStyle(fontSize: 20),)
+        ],
+      );
   }
 }
