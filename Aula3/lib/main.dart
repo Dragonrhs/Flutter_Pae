dce121b3511c6fb45cbde76b83b84a65df09459f
Ryuske <Ryuske2009@hotmail.com>
2024-04-05 13:39:08 -0300

    <feat>[]: texto appbar inicial

--- Aula3/lib/main.dart
+++ Aula3/lib/main.dart
@@ -5,5 +5,32 @@ void main() {
 }
 
 class App extends StatelessWidget {
+  const App({super.key});
   
+
+  @override
+  Widget build(BuildContext context){
+    return const MaterialApp(
+      title: 'Pagina',
+      home: Homepage(),
+    );
+  }
+}
+
+class Homepage extends StatelessWidget {
+
+  const Homepage({super.key});
+  
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        title: const Text('Flutter', style:TextStyle(color: Colors.white),),
+      ),
+      
+    );  
+      
+    
+  }
 }
