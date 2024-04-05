48417d55ed49e382044559e5b67c4c914250c6af
Ryuske <Ryuske2009@hotmail.com>
2024-04-05 14:49:36 -0300

    <feat>[]: statefulwidget

--- Aula3/lib/Widgets/Star_widget.dart
+++ Aula3/lib/Widgets/Star_widget.dart
@@ -4,17 +4,43 @@ import 'package:flutter/material.dart';
 
 
 
-class Starwidget extends StatelessWidget {
-  const Starwidget({Key? key}) : super(key: key);
+class Starwidget extends StatefulWidget {
+  
+
+  const Starwidget({super.key});
+
+  @override
+  State<Starwidget> createState() => _StarwidgetState();
+}
+
+class _StarwidgetState extends State<Starwidget> {
+  bool estaclicado = false;
 
   @override
   Widget build(BuildContext context) {
-    return  const Column(
+    
+    return   Column(
         children: [
-          Icon(Icons.star, color: Colors.yellow, size: 48,),
-          Text("+45.00 alunos", style: TextStyle(fontSize: 20),),
-          Text("Didatica garantida", style: TextStyle(fontSize: 20),)
+          IconButton(
+            onPressed: (){
+
+              setState((){
+                estaclicado = !estaclicado;
+              });}, 
+            icon: Icon(Icons.star, size:48, color: qualcor()),
+          ),
+          const Text("+45.00 alunos", style: TextStyle(fontSize: 20),),
+          const Text("Didatica garantida", style: TextStyle(fontSize: 20),)
         ],
       );
   }
+
+  qualcor(){
+    if(estaclicado){
+      return Colors.yellow;
+    }else{
+      return Colors.grey;
+    }
+  }
+
 }
