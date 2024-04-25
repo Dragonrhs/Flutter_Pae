bc4a3bc230c7acff6ff04886fc015be1da207633
Ryuske Hideaki Sato <ryuske2009@hotmail.com>
2024-04-25 10:52:20 -0300

    <fix>[]: home page divergente

--- entrega_2/lib/pages/home_page.dart
+++ entrega_2/lib/pages/home_page.dart
@@ -1,28 +1,45 @@
 import 'package:flutter/material.dart';
 
-class ItemComponent extends StatelessWidget {
-  final int contador;
-  final IconButton button;
-  const ItemComponent({super.key, required this.contador, required this.button});
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(16),
-      child: Container(
-        padding: const EdgeInsets.all(16),
-        decoration: BoxDecoration(
-            border: Border.all(color: Colors.red),
-            borderRadius: BorderRadius.circular(20)),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Text('Item ${contador.toString()}'),
-            button,
-          ],
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Home Page',
+          style: TextStyle(
+            color: Colors.white,
+          ),
+        ),
+        backgroundColor: Colors.red,
+      ),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 12),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text("Listagem e Navegação",
+                style: TextStyle(color: Colors.black,
+                  fontWeight: FontWeight.bold,fontSize: 32,
+                ),
+              ),
+          
+              const Text("  Uma aplicação para\n gerência de estado e\nnavegação com Flutter",style: TextStyle(fontSize: 18)),
+              TextButton(
+                style: ButtonStyle(backgroundColor: MaterialStateProperty.all(Colors.red)),
+                child: const Text('Ir para List Page',style: TextStyle(color: Colors.white,fontSize: 26),),
+                onPressed: () {
+                  Navigator.pushNamed(context, '/second');
+                },
+              ),
+            ],
+          ),
         ),
       ),
+
     );
   }
-  
 }
